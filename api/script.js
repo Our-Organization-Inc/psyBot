f0ab8b3a3bc4250a38c36626862ddbda47c6365a
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2021-01-08 02:47:44 +0500

    Optimized styles

--- api/script.js
+++ api/script.js
@@ -2,17 +2,19 @@
 * Created by Ekure
 * RegExp section
 * sun 03-01-'21
+* Last updated: Jan 8, 2021
 */
 var express = require("express"),
     app = express.Router({mergeParams:true});
 var usrname = "" 
 
-var regs = [ /how( do| should| would) i overcome this (disorder|condition)/gi,   // 0
-/(do|does) any\s?one (love|like|(care |think )about) me/gi,                      // 1
-/life (is|has) (not)?\s?(been)?\s?(always)?\s?(easy)?\s?for me/gi,               // 2
-/why (do|don\'?t) people( not)? (like|love|hate) me/gi,                          // 3
-/(why (am|do) I have?(ing)? nightmares|what causes nightmares).?/i,               // 4
-/(hi|h(e|o)l?lo)/i                                                                //5
+var regs = [ /how (do|should|would|can) i overcome (\w.+\s)?.+/i,                                                           // 0
+/(do|does) any\s?one (love|like|(care |think )about) me/i,                                                                  // 1
+/life (is|has) (not)?\s?(been)?\s?(always)?\s?(easy)?\s?for me/i,                                                           // 2
+/why (do|don\'?t) people( not)? (like|love|hate) me/i,                                                                      // 3
+/(why am I having nightmares|(causes of|what causes) nightmares|what are nightmares caused by)/i,                           // 4
+/^(hi|hello|greetings)/i,                                                                                                   // 5
+/(why am I sleepwalking|(causes of|what causes) nightmares|what are nightmares caused by)/i                                 // 6
 ]
 var answers = {
     "0":[""],
@@ -26,8 +28,9 @@ var answers = {
          "If you observe it hasn't been easy for anyone. \
           Even though try your best to be happy at all times."],
     "3": ["of course", "if others don\'t I do", "God does"],
-    "4":["It may be because of unfulfilled goals"],
-    "5":[`Hey ${usrname}`, `Hi ${usrname}`, `How are you ${usrname}`]
+    "4":[`Nightmares tend to be caused by stress, anxiety, or sometimes as a reaction to certain medications`],
+    "5":[`Hey ${usrname}. How are you feeling?`, `Hi ${usrname}. How are you? And What can I help you with?`, `How are you doing, ${usrname}?`],
+    "6":["Causes of sleepwalking include: Hereditary (the condition may run in families). Lack of sleep or extreme fatigue. Interrupted sleep or unproductive sleep, from disorders like sleep apnea (brief pauses in the child's breathing pattern during sleep)"]
 }
 
 
